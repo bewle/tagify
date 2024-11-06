2497e678bda540374a7c66f5587e807bb19bf7d8
bewle <everbelle@proton.me>
2024-11-06 13:47:43 -0500

    build(deps): update package.json, FileTree.tsx and yarn.lock

--- src/components/FileTree.tsx
+++ src/components/FileTree.tsx
@@ -11,20 +11,21 @@ export function FileTree() {
         <div className="flex flex-col gap-2">
             {files.map((file) => (
                 <div
-                    className="flex items-center gap-2"
+                    className="flex items-center gap-2 pl-2 rounded-md"
                     key={file.id}
                     style={{
                         backgroundColor:
                             selectedFile === file.id
-                                ? "hsl(var(--background))"
+                                ? "hsl(var(--muted))"
                                 : "inherit",
                     }}
                 >
-                    <Checkbox />
+                    <Checkbox className="bg-background" />
                     <Button
                         onClick={() => {
                             setSelectedFile(file.id);
-                            console.log(selectedFile);
+                            console.log("selected file:", selectedFile);
+                            console.log("file id:", file.id);
                         }}
                         variant="link"
                         className="justify-start max-w-full p-0 pr-6 font-mono text-[13px] tracking-tight"
