0643e75c292fab7951fe93d4e7f2dbc21e3bd07e
bewle <everbelle@proton.me>
2024-11-06 14:51:41 -0500

    update layout.tsx and page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,25 @@
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { FileTree } from "@/components/FileTree";
 import UploadButton from "@/components/UploadButton";
 import RemoveButton from "@/components/RemoveButton";
 
 export default function Page() {
     return (
-        <main className="container flex flex-1 h-full gap-4 ">
-            <Card className="w-1/4 transition-colors">
-                <CardHeader>
-                    <CardTitle className="flex items-center justify-between ">
-                        <p className="text-xl font-bold">loaded files</p>
-                        <div className="flex gap-2">
-                            <RemoveButton />
-                            <UploadButton />
-                        </div>
-                    </CardTitle>
-                </CardHeader>
-                <CardContent>
+        <main className="container flex flex-1 h-full ">
+            <div className="flex flex-col w-1/4 gap-4 p-6 transition-colors border-x border-border">
+                <div className="flex items-center justify-between ">
+                    <p className="text-xl font-bold">loaded files</p>
+                    <div className="flex gap-2">
+                        <RemoveButton />
+                        <UploadButton />
+                    </div>
+                </div>
+                <div>
                     <FileTree />
-                </CardContent>
-            </Card>
-            <Card className="flex-1 py-6 transition-colors">
-                <CardContent>test</CardContent>
-            </Card>
+                </div>
+            </div>
+            <div className="flex-1 p-6 transition-colors border-r border-border">
+                <div>test</div>
+            </div>
         </main>
     );
 }
