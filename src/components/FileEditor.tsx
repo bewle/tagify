a8ed7b602cb63cbc4f2e97ac19225f2aa96915e4
bewle <everbelle@proton.me>
2024-11-07 16:50:47 -0500

    update FileEditor.tsx

--- src/components/FileEditor.tsx
+++ src/components/FileEditor.tsx
@@ -1,23 +1,37 @@
+"use client";
+
 import { FileQuestion } from "lucide-react";
 import FileEditorForm from "./FileEditorForm";
 import { Card } from "./ui/card";
+import { useFilesStore } from "@/lib/store/files";
 
 export default function FileEditor() {
+    const { files } = useFilesStore();
+
     return (
         <>
-            <div className="absolute top-0 left-0 grid w-full h-full place-items-center">
-                {/* <div className="rounded-md bg-background size-64"></div> */}
-                <Card className="flex flex-col items-center justify-center gap-2 transition-colors rounded-lg shadow-lg size-64">
-                    <FileQuestion size={56} />
-                    <p className="text-xl font-semibold">no files loaded</p>
-                    <p className="text-sm text-muted-foreground max-w-[75%] text-center">
-                        load a file by dragging or selecting the upload button
-                    </p>
-                </Card>
-            </div>
-            <div className="opacity-30">
+            {files.length === 0 ? (
+                <>
+                    <div className="absolute top-0 left-0 grid w-full h-full place-items-center">
+                        {/* <div className="rounded-md bg-background size-64"></div> */}
+                        <Card className="flex flex-col items-center justify-center gap-2 transition-colors rounded-lg shadow-lg size-64">
+                            <FileQuestion size={56} />
+                            <p className="text-xl font-semibold">
+                                no files loaded
+                            </p>
+                            <p className="text-sm text-muted-foreground max-w-[75%] text-center">
+                                load a file by dragging or selecting the upload
+                                button
+                            </p>
+                        </Card>
+                    </div>
+                    <div className="opacity-30">
+                        <FileEditorForm />
+                    </div>
+                </>
+            ) : (
                 <FileEditorForm />
-            </div>
+            )}
         </>
     );
 }
