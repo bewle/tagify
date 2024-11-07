fbd3ead5da2351064a3e77696f55103e9a81f7d9
bewle <everbelle@proton.me>
2024-11-07 16:39:45 -0500

    update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,9 +2,12 @@ import ThemeToggle from "./ThemeToggle";
 
 export default function Navbar() {
     return (
-        <nav className="h-20 border-b-[1px] border-border shadow-sm">
+        <nav className="h-20 border-b-[1px] border-border shadow-sm transition-colors">
             <div className="container flex items-center justify-between h-full ">
                 <p className="text-3xl font-bold">tagify</p>
+                <p className="text-sm tracking-wider text-muted-foreground">
+                    v0.0.1. made with ❤️
+                </p>
                 <div className="flex gap-2">
                     <ThemeToggle variant="outline" />
                 </div>
