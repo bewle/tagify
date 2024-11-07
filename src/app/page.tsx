a08d4d7ecef67e2fb1842b6256d1b42aaa5814b2
bewle <everbelle@proton.me>
2024-11-07 17:01:24 -0500

    update 1 file and create 1 file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,26 +2,30 @@ import { FileTree } from "@/components/FileTree";
 import UploadButton from "@/components/UploadButton";
 import RemoveButton from "@/components/RemoveButton";
 import FileEditor from "@/components/FileEditor";
+import DropHandler from "@/components/DropHandler";
 
 export default function Page() {
     return (
-        <main className="container flex flex-1 h-full ">
-            <div className="flex flex-col w-1/4 gap-4 p-6 transition-colors border-x border-border">
-                <div className="flex items-center justify-between ">
-                    <p className="text-xl font-bold">loaded files</p>
-                    <div className="flex gap-2">
-                        <RemoveButton />
-                        <UploadButton />
+        <>
+            <DropHandler />
+            <main className="container flex flex-1 h-full ">
+                <div className="flex flex-col w-1/4 gap-4 p-6 transition-colors border-x border-border">
+                    <div className="flex items-center justify-between ">
+                        <p className="text-xl font-bold">loaded files</p>
+                        <div className="flex gap-2">
+                            <RemoveButton />
+                            <UploadButton />
+                        </div>
+                    </div>
+                    {/* <GetStartedText /> */}
+                    <div className="max-h-[calc(100vh-10rem)] overflow-y-auto overflow-x-hidden ">
+                        <FileTree />
                     </div>
                 </div>
-                {/* <GetStartedText /> */}
-                <div className="max-h-[calc(100vh-10rem)] overflow-y-auto overflow-x-hidden ">
-                    <FileTree />
+                <div className="relative flex-1 p-6 transition-colors border-r border-border">
+                    <FileEditor />
                 </div>
-            </div>
-            <div className="relative flex-1 p-6 transition-colors border-r border-border">
-                <FileEditor />
-            </div>
-        </main>
+            </main>
+        </>
     );
 }
---
+++ src/components/DropHandler.tsx
@@ -0,0 +1,3 @@
+export default function DropHandler() {
+    return <div className="hidden"></div>;
+}
