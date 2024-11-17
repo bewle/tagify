f0c3904141d90d2c50c344bde44e4fbc49941204
less <everbelle@proton.me>
2024-11-16 19:31:31 -0500

    update package.json and Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,9 +5,16 @@ export default function Navbar() {
         <nav className="h-20 border-b-[1px] border-border shadow-sm transition-colors">
             <div className="container flex items-center justify-between h-full ">
                 <p className="text-3xl font-bold">tagify</p>
-                <p className="text-sm tracking-wider text-muted-foreground">
-                    {process.env.VERSION} made with ❤️
-                </p>
+                <div className="flex items-center gap-2">
+                    <p>made with ❤️</p>
+                    <p className="text-sm tracking-wider text-muted-foreground">
+                        v{process.env.VERSION} (
+                        <span className="text-[10px]">
+                            {process.env.COMMIT_HASH}
+                        </span>
+                        )
+                    </p>
+                </div>
                 <div className="flex gap-2">
                     <ThemeToggle variant="outline" />
                 </div>
