0a5daa34b5df0b3fca4a64bf9699fa69f1262f60
bewle <everbelle@proton.me>
2024-11-24 09:58:56 -0500

    update next.config.js and layout.tsx

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
  * for Docker builds.
  */
 import { execSync } from "child_process";
-import packageJson from "./package.json" assert { type: "json" };
+import packageJson from "./package.json" with { type: "json" };
 import "./src/env.js";
 
 const commitHash = execSync("git log --pretty=format:%h -n1")
